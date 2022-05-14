09e28a9885b123aab6603d4dcb78b47008e99828
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-05-14 13:17:04 +0100

    crud app fully implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import ReadPost from './pages/ReadPosts';
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<ReadPost/>}/>
+        <Route path='/readposts' element={<ReadPost/>} />
+        <Route path='/create' element={<CreatePost/>} />
+        <Route path='/readposts/:id' element={<UpdatePost/>} />
+      </Routes>
     </div>
   );
 }

99ec8fe2fe22e455ad7f770ee303be7130871239
Toochukwu Okolie <charlestoochukwu5@gmail.com>
2022-05-14 13:28:32 +0100

    hashrouter used

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
 
